62069bab82c697199e08006caf6f49ca3ec7eae8
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2023-08-30 22:33:45 -0300

    rangesIfsELoopsComChar.kt

---
+++ src/main/kotlin/controleDeFluxoEColecoes/controleDeFluxo/rangesIfsELoopsComChar.kt
@@ -0,0 +1,32 @@
+package controleDeFluxoEColecoes.controleDeFluxo
+
+fun main() {
+
+    //Range com char
+
+    for (c in 'a'..'d') {
+        print("$c ")
+    }
+
+    print(" -  ")
+
+    for (c in 'z' downTo 's' step 2) {
+        print("$c ")
+    }
+
+
+    //Range em if
+
+    val x = 2
+
+        //Se x estiver no intervalo de 1 a 5 inclusive
+    if (x in 1..5) {
+        print("$x está entre 1 a 5 inclusive")
+    }
+
+        //Se x não estiver no intervalo de 6 a 10 inclusive
+    if (x !in 6..10) { // !in -> não é/está
+        print("$x não está entre 6 a 10 inclusive")
+    }
+
+}
